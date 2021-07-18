6dd3e8f292f37303124d2f10637067592922de7a
Daniel Esteban Nombela <dani@gatunes.com>
2021-07-18 02:17:39 +0200

    Refactor worldgen program

--- src/worldgen.c
+++ src/worldgen.c
@@ -1,40 +1,48 @@
 #define FNL_IMPL
 #include "../vendor/FastNoiseLite/C/FastNoiseLite.h"
 
-static const unsigned int getColorFromNoise(unsigned char noise) {
+typedef struct {
+  unsigned char value;
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+} Voxel;
+
+fnl_state noise;
+
+static void setColorFromNoise(
+  Voxel* voxel,
+  unsigned char noise
+) {
   noise = 255 - noise;
   if (noise < 85) {
-    return (
-      ((255 - noise * 3) << 16)
-      | (0 << 8)
-      | (noise * 3)
-    );
+    voxel->r = (255 - noise * 3);
+    voxel->g = 0;
+    voxel->b = noise * 3;
+    return;
   }
   if (noise < 170) {
     noise -= 85;
-    return (
-      (0 << 16)
-      | ((noise * 3) << 8)
-      | (255 - noise * 3)
-    );
+    voxel->r = 0;
+    voxel->g = noise * 3;
+    voxel->b = 255 - noise * 3;
+    return;
   }
   noise -= 170;
-  return (
-    ((noise * 3) << 16)
-    | ((255 - noise * 3) << 8)
-    | 0
-  );
+  voxel->r = noise * 3;
+  voxel->g = 255 - noise * 3;
+  voxel->b = 0;
 }
 
 void run(
-  unsigned char* chunk,
+  Voxel* chunk,
   unsigned char chunkSize,
   const int seed,
   int chunkX,
   int chunkY,
   int chunkZ
 ) {
-  fnl_state noise = fnlCreateState();
+  noise = fnlCreateState();
   noise.seed = seed;
   noise.fractal_type = FNL_FRACTAL_FBM;
   chunkX *= chunkSize;
@@ -42,14 +50,14 @@ void run(
   chunkZ *= chunkSize;
   for (int i = 0, z = chunkZ; z < chunkZ + chunkSize; z++) {
     for (int y = chunkY; y < chunkY + chunkSize; y++) {
-      for (int x = chunkX; x < chunkX + chunkSize; x++, i += 4) {
-        const unsigned int color = getColorFromNoise(
-          fabs(fnlGetNoise3D(&noise, z, x, y)) * 255
+      for (int x = chunkX; x < chunkX + chunkSize; x++, i++) {
+        chunk[i].value = (
+          0xFF - (fabs(fnlGetNoise3D(&noise, x * 1.5f, y * 1.5f, z * 1.5f)) * 0xFF)
         );
-        chunk[i] = 255 - (fabs(fnlGetNoise3D(&noise, x * 1.5f, y * 1.5f, z * 1.5f)) * 255);
-        chunk[i + 1] = (int) (color >> 16) & 0xFF;
-        chunk[i + 2] = (int) (color >> 8) & 0xFF;
-        chunk[i + 3] = (int) (color & 0xFF);
+        setColorFromNoise(
+          &chunk[i],
+          fabs(fnlGetNoise3D(&noise, z, x, y)) * 0xFF
+        );
       }
     }
   }
