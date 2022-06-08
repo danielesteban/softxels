a029528793ffc304801db52a72ae1542dd8cd478
Daniel Esteban Nombela <dani@gatunes.com>
2022-06-08 22:51:28 +0200

    Bump to v0.0.36

--- rollup.config.js
+++ rollup.config.js
@@ -36,7 +36,7 @@ export default {
           author: 'Daniel Esteban Nombela',
           license: 'MIT',
           module: 'softxels.js',
-          version: '0.0.35',
+          version: '0.0.36',
           homepage: 'https://softxels.gatunes.com',
           repository: {
             type: 'git',
