dfa942c12b7f2b0c1c7bbe97454ee35a783187bd
Daniel Esteban Nombela <dani@gatunes.com>
2022-06-09 14:06:49 +0200

    Help the garbage collector

--- rollup.config.js
+++ rollup.config.js
@@ -36,7 +36,7 @@ export default {
           author: 'Daniel Esteban Nombela',
           license: 'MIT',
           module: 'softxels.js',
-          version: '0.0.36',
+          version: '0.0.37',
           homepage: 'https://softxels.gatunes.com',
           repository: {
             type: 'git',
